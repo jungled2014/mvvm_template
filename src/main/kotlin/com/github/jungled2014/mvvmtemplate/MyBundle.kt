0ebcf5b7356e7d84a66a3f78dc4e8e9ea2d31d44
GitHub Action <action@github.com>
2021-03-26 05:57:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/jungled2014/mvvmtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/jungled2014/mvvmtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jungled2014.mvvmtemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jungled2014/mvvmtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jungled2014/mvvmtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jungled2014.mvvmtemplate.services
 
+import com.github.jungled2014.mvvmtemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
